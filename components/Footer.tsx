1834a805975c2f2a0ad92b7f576cfbedab1c60b1
CodeSpidey-web <codespidey@example.com>
2026-07-23 04:59:05 +0530

    fix: responsive footer alignment and add green WhatsApp chat button to mobile toolbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,21 +7,21 @@ const Footer = () => {
                 <div className="rbt-footer-top rbt-section-gap2">
                     <div className="container">
                         <div className="row mt_dec--24 justify-content-between">
-                            {/* Column 1: Logo, Description & Social */}
-                            <div className="col-lg-4 col-md-6 col-sm-12 col-12 mt--24">
+                            {/* Column 1: Logo, Description & Contact */}
+                            <div className="col-lg-4 col-md-6 col-sm-12 col-12 mt--24 text-center text-md-start">
                                 <div className="footer-widget">
-                                    <div className="logo" style={{ marginLeft: '-15px' }}>
+                                    <div className="logo d-flex justify-content-center justify-content-md-start">
                                         <a href="/">
                                             <img alt="Ocean Student Projects Logo" src="/assets/images/logo/transperent_logo.webp" style={{ maxHeight: '80px' }} />
                                         </a>
                                     </div>
-                                    <p className="description mt--12">
+                                    <p className="description mt--16">
                                         Your one-stop destination for electronics components, development boards, and student projects in India.
                                     </p>
-                                    <ul className="ft-link mt--16">
+                                    <ul className="ft-link mt--16 p-0 list-unstyled">
                                         <li>
-                                            <a href="tel:+917338975699">
-                                                <i className="fa-regular fa-phone mr--8"></i>+91 733 897 5699
+                                            <a href="tel:+917338975699" className="d-inline-flex align-items-center">
+                                                <i className="fa-regular fa-phone mr--8 text-primary"></i>+91 733 897 5699
                                             </a>
                                         </li>
                                     </ul>
@@ -29,10 +29,10 @@ const Footer = () => {
                             </div>
 
                             {/* Column 2: Get to Know Us */}
-                            <div className="col-lg-2 col-md-6 col-sm-6 col-12 mt--24">
+                            <div className="col-lg-2 col-md-6 col-sm-6 col-12 mt--24 text-center text-md-start">
                                 <div className="footer-widget rbt-link-hover">
                                     <h3 className="ft-title">Get to Know Us</h3>
-                                    <ul className="ft-link">
+                                    <ul className="ft-link p-0 list-unstyled">
                                         <li><a href="/about">About Us</a></li>
                                         <li><a href="/contact">Contact Us</a></li>
                                         <li><a href="/cart">Shopping Cart</a></li>
@@ -41,10 +41,10 @@ const Footer = () => {
                             </div>
 
                             {/* Column 3: Shop Categories */}
-                            <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt--24">
+                            <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt--24 text-center text-md-start">
                                 <div className="footer-widget rbt-link-hover">
                                     <h3 className="ft-title">Shop Categories</h3>
-                                    <ul className="ft-link">
+                                    <ul className="ft-link p-0 list-unstyled">
                                         <li><a href="/shop">Development Boards</a></li>
                                         <li><a href="/shop">Sensors &amp; Modules</a></li>
                                         <li><a href="/shop">IoT &amp; DIY Starter Kits</a></li>
@@ -54,10 +54,10 @@ const Footer = () => {
                             </div>
 
                             {/* Column 4: Help & Policies */}
-                            <div className="col-lg-3 col-md-6 col-sm-12 col-12 mt--24">
+                            <div className="col-lg-3 col-md-6 col-sm-12 col-12 mt--24 text-center text-md-start">
                                 <div className="footer-widget rbt-link-hover">
                                     <h3 className="ft-title">Let Us Help You</h3>
-                                    <ul className="ft-link">
+                                    <ul className="ft-link p-0 list-unstyled">
                                         <li><a href="#">Returns Policies</a></li>
                                         <li><a href="#">Refund and Returns</a></li>
                                         <li><a href="#">Privacy Policy</a></li>
@@ -78,11 +78,11 @@ const Footer = () => {
                 {/* Start Copyright Area  */}
                 <div className="copyright-area copyright-style-1">
                     <div className="container">
-                        <div className="row mt_dec--24 align-items-center justify-content-between">
-                            <div className="col-lg-4 col-md-12 col-12 mt--24">
+                        <div className="row mt_dec--24 align-items-center justify-content-between text-center">
+                            <div className="col-lg-4 col-md-12 col-12 mt--24 text-lg-start">
                                 <div className="rbt-footer-social-area justify-content-center justify-content-lg-start d-flex flex-wrap rbt-gap--16">
                                     <p className="title mb--0">Follow Us :</p>
-                                    <ul className="rbt-social-icon-list">
+                                    <ul className="rbt-social-icon-list p-0 m-0 list-unstyled">
                                         <li><a href="#"><i className="fa-brands fa-x-twitter"></i></a></li>
                                         <li><a href="#"><i className="fa-brands fa-youtube"></i></a></li>
                                         <li><a href="#"><i className="fa-brands fa-facebook"></i></a></li>
@@ -93,10 +93,10 @@ const Footer = () => {
                                 </div>
                             </div>
                             <div className="col-lg-4 col-md-12 col-12 mt--24">
-                                <p className="rbt-link-hover text-center">Copyright <span className="copyright-year">2026</span> © <a className="rbt-text-bold rbt-text-color-heading" href="/">Ocean Student Projects</a></p>
+                                <p className="rbt-link-hover mb-0">Copyright <span className="copyright-year">2026</span> © <a className="rbt-text-bold rbt-text-color-heading" href="/">Ocean Student Projects</a></p>
                             </div>
-                            <div className="col-lg-4 col-md-12 col-12 mt--24">
-                                <ul className="payment-img-link justify-content-center justify-content-lg-end">
+                            <div className="col-lg-4 col-md-12 col-12 mt--24 text-lg-end">
+                                <ul className="payment-img-link justify-content-center justify-content-lg-end p-0 m-0 list-unstyled">
                                     <li><a href="#"><img alt="Payment Brand Image" src="/assets/images/payment-brand/image-01.webp"/></a></li>
                                 </ul>
                             </div>
@@ -123,6 +123,14 @@ const Footer = () => {
                                     <a href="/shop" className="rbt-round-btn has-rbt-md-fsize">
                                         <i className="fa-regular fa-bag-shopping"></i>
                                         <span className="rbt-toolbar-label"> Shop</span>
+                                    </a>
+                                </li>
+
+                                {/* Highly useful WhatsApp Support shortcut */}
+                                <li className="rbt-access-box">
+                                    <a className="rbt-round-btn has-rbt-md-fsize" href="https://wa.me/917338975699?text=Hi,%20I%20am%20interested%20in%20buying%20electronics%20components%20from%20Ocean%20Student%20Projects." target="_blank" rel="noopener noreferrer" style={{ backgroundColor: '#e8f7f0', color: '#136c39' }}>
+                                        <i className="fa-brands fa-whatsapp" style={{ fontSize: '1.2rem' }}></i>
+                                        <span className="rbt-toolbar-label" style={{ color: '#136c39', fontWeight: 'bold' }}> WhatsApp</span>
                                     </a>
                                 </li>
 
