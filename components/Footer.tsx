37e22d00f816e1322519684d8b51366724296dc2
CodeSpidey-web <codespidey@example.com>
2026-07-23 04:55:50 +0530

    fix: refactor footer to 4 columns and clean up mobile bottom toolbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,193 +4,147 @@ const Footer = () => {
     return (
         <>
             <footer className="rbt-footer rbt-footer-style-ten rbt-bg-color-gray-light">
-<div className="rbt-footer-top rbt-section-gap2">
-<div className="container">
+                <div className="rbt-footer-top rbt-section-gap2">
+                    <div className="container">
+                        <div className="row mt_dec--24 justify-content-between">
+                            {/* Column 1: Logo, Description & Social */}
+                            <div className="col-lg-4 col-md-6 col-sm-12 col-12 mt--24">
+                                <div className="footer-widget">
+                                    <div className="logo" style={{ marginLeft: '-15px' }}>
+                                        <a href="/">
+                                            <img alt="Ocean Student Projects Logo" src="/assets/images/logo/transperent_logo.webp" style={{ maxHeight: '80px' }} />
+                                        </a>
+                                    </div>
+                                    <p className="description mt--12">
+                                        Your one-stop destination for electronics components, development boards, and student projects in India.
+                                    </p>
+                                    <ul className="ft-link mt--16">
+                                        <li>
+                                            <a href="tel:+917338975699">
+                                                <i className="fa-regular fa-phone mr--8"></i>+91 733 897 5699
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
 
-<div className="row">
-<div className="col-12">
-<div className="rbt-watermark-text rbt-watermark-color-var-1 text-center">
-<span>Follow Us</span>
-</div>
-</div>
-</div>
-<div className="row mt_dec--24">
-<div className="col-12">
-<hr className="rbt-separator m-0"/>
-</div>
-</div>
-            <div className="row row--12 rbt-footer-nav-warpper">
-              <div className="col" style={{marginTop: '-30px'}}>
-                <div className="footer-widget">
-                  <div className="logo" style={{marginLeft: '-40px'}}>
-                    <a href="/">
-                      <img alt="Ocean Student Projects Logo" src="/assets/images/logo/transperent_logo.webp"/>
-                    </a>
-                  </div>
-                  <p className="description" style={{marginTop: '-20px', marginLeft: '-30px'}}>
-                    Your one-stop destination for electronics components, development boards, and student projects in India.
-                  </p>
-                </div>
-              </div>
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">Let Us Help You</h3>
-                  <ul className="ft-link">
-                    <li><a href="account-info.html">Account Info</a></li>
-                    <li><a href="my-order-history.html">Your Orders</a></li>
-                    <li><a href="return-policy.html">Returns Policies</a></li>
-                    <li><a href="my-order-history.html">Shipping Rates</a></li>
-                    <li><a href="return-policy.html">Refund and Returns</a></li>
-                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                    <li><a href="terms-policy.html">Terms and Conditions</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">Get to Know Us</h3>
-                  <ul className="ft-link">
-                    <li><a href="about.html">About Us</a></li>
-                    <li><a href="about.html">Customer reviews</a></li>
-                  </ul>
-                </div>
-              </div>
+                            {/* Column 2: Get to Know Us */}
+                            <div className="col-lg-2 col-md-6 col-sm-6 col-12 mt--24">
+                                <div className="footer-widget rbt-link-hover">
+                                    <h3 className="ft-title">Get to Know Us</h3>
+                                    <ul className="ft-link">
+                                        <li><a href="/about">About Us</a></li>
+                                        <li><a href="/contact">Contact Us</a></li>
+                                        <li><a href="/cart">Shopping Cart</a></li>
+                                    </ul>
+                                </div>
+                            </div>
 
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">Shop Categories</h3>
-                  <ul className="ft-link">
-                    <li><a href="/shop">Arduino Boards</a></li>
-                    <li><a href="/shop">Raspberry Pi &amp; SBCs</a></li>
-                    <li><a href="/shop">Sensors &amp; Modules</a></li>
-                    <li><a href="/shop">Displays &amp; Screens</a></li>
-                    <li><a href="/shop">DIY Kits &amp; Robotics</a></li>
-                    <li><a href="/shop">Electronic Components</a></li>
-                    <li><a href="/shop">All Products</a></li>
-                  </ul>
+                            {/* Column 3: Shop Categories */}
+                            <div className="col-lg-3 col-md-6 col-sm-6 col-12 mt--24">
+                                <div className="footer-widget rbt-link-hover">
+                                    <h3 className="ft-title">Shop Categories</h3>
+                                    <ul className="ft-link">
+                                        <li><a href="/shop">Development Boards</a></li>
+                                        <li><a href="/shop">Sensors &amp; Modules</a></li>
+                                        <li><a href="/shop">IoT &amp; DIY Starter Kits</a></li>
+                                        <li><a href="/shop">All Products</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            {/* Column 4: Help & Policies */}
+                            <div className="col-lg-3 col-md-6 col-sm-12 col-12 mt--24">
+                                <div className="footer-widget rbt-link-hover">
+                                    <h3 className="ft-title">Let Us Help You</h3>
+                                    <ul className="ft-link">
+                                        <li><a href="#">Returns Policies</a></li>
+                                        <li><a href="#">Refund and Returns</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                        <li><a href="#">Terms and Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">My Account</h3>
-                  <ul className="ft-link">
-                    <li><a href="signin.html">Sign In</a></li>
-                    <li><a href="signup.html">Create Account</a></li>
-                    <li><a href="cart.html">Shopping Cart</a></li>
-                  </ul>
+
+                <div className="rbt-separator-mid">
+                    <div className="container">
+                        <hr className="rbt-separator m-0"/>
+                    </div>
                 </div>
-              </div>
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">Help Center</h3>
-                  <ul className="ft-link">
-                    <li><a href="/contact">Contact Us</a></li>
-                    <li><a href="#">FAQs</a></li>
-                    <li><a href="#">Find a Store</a></li>
-                    <li><a href="#">Shipping Info</a></li>
-                    <li><a href="#">Payment Methods</a></li>
-                  </ul>
+
+                {/* Start Copyright Area  */}
+                <div className="copyright-area copyright-style-1">
+                    <div className="container">
+                        <div className="row mt_dec--24 align-items-center justify-content-between">
+                            <div className="col-lg-4 col-md-12 col-12 mt--24">
+                                <div className="rbt-footer-social-area justify-content-center justify-content-lg-start d-flex flex-wrap rbt-gap--16">
+                                    <p className="title mb--0">Follow Us :</p>
+                                    <ul className="rbt-social-icon-list">
+                                        <li><a href="#"><i className="fa-brands fa-x-twitter"></i></a></li>
+                                        <li><a href="#"><i className="fa-brands fa-youtube"></i></a></li>
+                                        <li><a href="#"><i className="fa-brands fa-facebook"></i></a></li>
+                                        <li><a href="#"><i className="fa-brands fa-whatsapp"></i></a></li>
+                                        <li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
+                                        <li><a href="#"><i className="fa-brands fa-telegram"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-lg-4 col-md-12 col-12 mt--24">
+                                <p className="rbt-link-hover text-center">Copyright <span className="copyright-year">2026</span> © <a className="rbt-text-bold rbt-text-color-heading" href="/">Ocean Student Projects</a></p>
+                            </div>
+                            <div className="col-lg-4 col-md-12 col-12 mt--24">
+                                <ul className="payment-img-link justify-content-center justify-content-lg-end">
+                                    <li><a href="#"><img alt="Payment Brand Image" src="/assets/images/payment-brand/image-01.webp"/></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="col mt--24">
-                <div className="footer-widget rbt-link-hover">
-                  <h3 className="ft-title">Contact</h3>
-                  <ul className="ft-link">
-                    <li><a href="tel:+917338975699">+91 733 897 5699</a></li>
-                  </ul>
+                {/* End Copyright Area  */}
+            </footer>
+
+            {/* Start Mobile bottom toolbar */}
+            <div className="rbt-toolbar rbt-toolbar--bottom d-block d-xl-none">
+                <div className="container p--0">
+                    <div className="row row--0">
+                        <div className="col-md-12">
+                            <ul className="rbt-quick-access onepagenav justify-content-around">
+                                <li className="rbt-access-box">
+                                    <a className="rbt-round-btn has-rbt-md-fsize" href="/">
+                                        <i className="fa-regular fa-house"></i>
+                                        <span className="rbt-toolbar-label"> Home</span>
+                                    </a>
+                                </li>
+
+                                <li className="rbt-access-box">
+                                    <a href="/shop" className="rbt-round-btn has-rbt-md-fsize">
+                                        <i className="fa-regular fa-bag-shopping"></i>
+                                        <span className="rbt-toolbar-label"> Shop</span>
+                                    </a>
+                                </li>
+
+                                <li className="rbt-access-box">
+                                    <a className="rbt-round-btn has-rbt-md-fsize" href="/cart">
+                                        <i className="fa-regular fa-cart-shopping"></i>
+                                        <span className="rbt-toolbar-label"> Cart</span>
+                                    </a>
+                                </li>
+
+                                <li className="rbt-access-box">
+                                    <a className="rbt-round-btn has-rbt-md-fsize" href="/contact">
+                                        <i className="fa-regular fa-envelope"></i>
+                                        <span className="rbt-toolbar-label"> Contact</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-</div>
-</div>
-<div className="rbt-separator-mid">
-<div className="container">
-<hr className="rbt-separator m-0"/>
-</div>
-</div>
-{/* Start Copyright Area  */}
-<div className="copyright-area copyright-style-1">
-<div className="container">
-<div className="row mt_dec--24 align-items-center justify-content-between">
-<div className="col-xxl-4 col-xl-3 col-lg-4 col-md-12 col-12 mt--24">
-<div className="rbt-footer-social-area justify-content-center justify-content-lg-start d-flex flex-wrap rbt-gap--16">
-<p className="title mb--0">Follow Us :</p>
-<ul className="rbt-social-icon-list">
-<li><a href="#"><i className="fa-brands fa-x-twitter"></i></a></li>
-<li><a href="#"><i className="fa-brands fa-youtube"></i></a></li>
-<li><a href="#"><i className="fa-brands fa-facebook"></i></a></li>
-<li><a href="#"><i className="fa-brands fa-whatsapp"></i></a></li>
-<li><a href="#"><i className="fa-brands fa-instagram"></i></a></li>
-<li><a href="#"><i className="fa-brands fa-telegram"></i></a></li>
-</ul>
-</div>
-</div>
-<div className="col-xxl-3 col-xl-4 col-lg-4 col-md-12 col-12 mt--24">
-<p className="rbt-link-hover text-center text-lg-start">Copyright <span className="copyright-year">2025</span> © <a className="rbt-text-bold rbt-text-color-heading" href="https://rainbowthemes.net/">Ocean Student Projects</a></p>
-</div>
-<div className="col-xxl-4 col-xl-4 col-lg-4 col-md-12 col-12 mt--24">
-<ul className="payment-img-link justify-content-center justify-content-lg-start justify-content-xl-end">
-<li><a href="#"><img alt="Payment Brand Image" src="/assets/images/payment-brand/image-01.webp"/></a></li>
-</ul>
-</div>
-</div>
-</div>
-</div>
-{/* End Copyright Area  */}
-</footer>
-
-{/* Start Mobile bottom toolbar */}
-<div className="rbt-toolbar rbt-toolbar--bottom d-block d-xl-none">
-    <div className="container p--0">
-        <div className="row row--0">
-            <div className="col-md-12">
-                <ul className="rbt-quick-access onepagenav">
-                    <li className="rbt-access-box">
-                        <a href="/shop" className="rbt-round-btn has-rbt-md-fsize">
-                            <i className="fa-regular fa-bag-shopping"></i>
-                            <span className="rbt-toolbar-label"> Shop</span>
-                        </a>
-                    </li>
-
-                    <li className="rbt-access-box rbt-wishlist">
-                        <a className="rbt-round-btn has-rbt-md-fsize" href="#!" data-bs-toggle="modal"
-                            data-bs-target="#wishlistModal">
-                            <i className="fa-regular fa-heart"></i>
-                            <div className="access-box-count">3</div>
-                            <span className="rbt-toolbar-label"> Wishlist</span>
-                        </a>
-                    </li>
-
-                    <li className="rbt-access-box">
-                        <a className="rbt-common-search-trigger-active rbt-round-btn has-rbt-md-fsize rbt-modern-close-btn"
-                            href="/">
-                            <i className="fa-regular fa-house search-icon"></i>
-                            <div className="modern-close-wrapper"></div>
-                            <span className="rbt-toolbar-label"> Home</span>
-                        </a>
-                    </li>
-
-                    <li className="rbt-access-box">
-                        <a href="#!" className="rbt-round-btn has-rbt-md-fsize rbt-compare-bottom-sidenav-activation">
-                            <i className="fa-regular fa-code-compare"></i>
-                            <div className="access-box-count">4</div>
-                            <span className="rbt-toolbar-label"> Compare</span>
-                        </a>
-                    </li>
-
-                    <li className="rbt-access-box">
-                        <a href="#!" className="rbt-round-btn has-rbt-md-fsize" data-bs-toggle="modal"
-                            data-bs-target="#signinModal">
-                            <i className="fa-regular fa-user"></i>
-                            <span className="rbt-toolbar-label"> Profile</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-{/* End Mobile bottom toolbar */}
+            {/* End Mobile bottom toolbar */}
         </>
     );
 };
